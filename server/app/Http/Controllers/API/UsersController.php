bf32f8e76203323a280890e376bf4c3a7a10aafd
Olloshukur6092 <matnazarovolloshukur7223@gmail.com>
2022-04-16 12:32:33 +0500

    Laravel and React projects

---
+++ server/app/Http/Controllers/API/UsersController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    // Register controller 
+    public function register() {
+        echo 'register qism';
+    }
+}
